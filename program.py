5256103da69f0d9afc04e83556791b01b4f46005
kelvinamesh <kelvinamenya@gmail.com>
2023-11-21 13:02:16 +0300

    verssion1

---
+++ program.py
@@ -0,0 +1,5 @@
+my_dict = dict()
+for i in range (3):
+  for j in range(2):
+    my_dict[i] = j
+print(my_dict)
---
+++ work.py
@@ -0,0 +1,4 @@
+numbers=[2,4,5,6]
+for i in range(len(numbers)):
+    numbers[i] =numbers[i] * 2
+print(numbers)
